644a6da27243e7f8a9c84f180dd9090558b90949
DevidB <devid10ba@outlook.com>
2022-05-02 19:31:09 -0700

    defs

--- TEXT_MAIN.py
+++ TEXT_MAIN.py
@@ -60,13 +60,7 @@ def spider():
     print("run back. You attempt to mske your way to the fork in the forest road")
     print("fight. ypu fight the spiders")
     print("bribe.")
-<<<<<<< HEAD
     spider_armed = False
-    
-    while True:
-        choice = input("> ")
-=======
-    spider_armed == False
     
     while True:
         choice = input("> ")
@@ -76,8 +70,7 @@ def spider():
         elif choice == "fight" and not spider_armed:
             print("You take out your sword and fight")
             print("you vanquished the Spider")
-            spider_armed = True    
->>>>>>> main
+            spider_armed = True   
 
         if choice == "run foward":
             death("The Spider webs you and takes you into the trees never to be seen again")
